7b0b9ed88e5a86d0044860134e80a2bb7b8b9efa
UPASANA BHANDARY <67952857+upasanabhandary23@users.noreply.github.com>
2022-03-31 16:52:32 +0530

    Add files via upload

---
+++ tasks2/search_dom.js
@@ -0,0 +1,22 @@
+let table = document.getElementById('age-table')
+
+table.getElementsByTagName('label')
+
+document.querySelectorAll('#age-table label')
+
+table.rows[0].cells[0]
+
+table.getElementsByTagName('td')[0]
+
+table.querySelector('td')
+
+let form = document.getElementsByName('search')[0]
+
+document.querySelector('form[name="search"]')
+
+form.getElementsByTagName('input')[0]
+
+form.querySelector('input')
+
+let inputs = form.querySelectorAll('input') 
+inputs[inputs.length-1] 
